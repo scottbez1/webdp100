9f86cf3dd348412438e60c61ae670b86848c47da
Scott Bezek <scottbez1@gmail.com>
2023-12-17 17:19:46 -0800

    [CI] Try suggested pages deploy process

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
-    assetPrefix,
-    basePath,
     output: 'export',
 }
 
